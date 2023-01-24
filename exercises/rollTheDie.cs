effb6ed1367b5ff3a13c8adec7deac12aa85bda7
arieldonner <arieldonner@gmail.com>
2023-01-24 21:59:53 +0000

    Completed Roll the Die!

---
+++ exercises/rollTheDie.cs
@@ -0,0 +1,14 @@
+using System;
+public class Player
+{
+    public int RollDie()
+    {
+        var rand = new Random();
+        return rand.Next(1, 18);
+    }
+    public double GenerateSpellStrength()
+    {
+        var rand = new Random();
+        return rand.NextDouble();
+    }
+}
